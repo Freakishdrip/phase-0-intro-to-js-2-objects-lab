1a12df70f021237ae43f2fd0218b81a98291fd24
Nick Drummond <nckdrmmnd@gmail.com>
2023-12-05 13:47:13 -0700

    array hay day

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+const employee = {
+    name: "Sam",
+    streetAddress: "11 Broadway"
+};
+
+function updateEmployeeWithKeyAndValue(obj,key,value){
+    const newObj = { ...employee };
+    newObj[key] = value;
+    return newObj;
+};
+
+const newEmployee = updateEmployeeWithKeyAndValue(
+    employee,
+    "name",
+    "Robert"
+);
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj,key,value){
+    obj[key] = value;
+    return obj;
+}
+const newerEmployee = destructivelyUpdateEmployeeWithKeyAndValue(employee, 'name', 'Bryon');
+
+function deleteFromEmployeeByKey(obj,key,value){
+    const newObj = { ...employee };
+    newObj[key] = value;
+    return newObj;
+}
+const delEmployee = deleteFromEmployeeByKey()
+
+function destructivelyDeleteFromEmployeeByKey(obj,key,value){
+    delete obj[key]
+    return obj;
+}
